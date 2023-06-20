5847dbeb8a3b98fe4328544e46e989cc67abd768
mostafa ahmed <mostafa.ahmed2509@gmail.com>
2023-06-20 06:47:11 +0300

    finished CRUD

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,7 @@
 
 const productsModel = require('../models/productsModel');
+let yup = require('yup');
+
 
 exports.getProducts  =  (req,res) => {
 
@@ -17,18 +19,54 @@ exports.getProductById  =  (req,res) => {
 
 exports.createProduct  =  (req,res) => {
 
-    let p = req.body;
+    let product = req.body;
     
-    res.send( productsModel.create(p) );
+    const vScheme = yup.object({
+        title: yup.string().required(),
+        price: yup.number().required(),
+        description: yup.string().required(),
+        categoryId: yup.number().required(),
+        images: yup.array().required()
+    }).noUnknown();
+
+    try{
+        vScheme.validateSync(product, {
+            strict: true,
+          });
+
+        res.send( productsModel.create(product) );
+    }
+    catch(error){
+        res.send(error.toString());
+    }
        
 }
 
 exports.updateProduct =  (req,res) => {
     
     let id = req.params.id;
-    let p = req.body;
+    let product = req.body;
+
+    const vScheme = yup.object({
+        title: yup.string(),
+        price: yup.number(),
+        description: yup.string(),
+        categoryId: yup.number(),
+        images: yup.array()
+    }).noUnknown();
+
+    try{
+        vScheme.validateSync(product, {
+            strict: true,
+          });
+
+        res.send( productsModel.update(id,product));
+    }
+    catch(error){
+        res.send(error.toString());
+    }
     
-    res.send( productsModel.update(id,p) );
+    
        
 }
 
--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,5 +1,6 @@
 
 let products = [];
+let categories = [];
 
 exports.getApiData = async (req,resUser) => {
     try {
@@ -7,6 +8,10 @@ exports.getApiData = async (req,resUser) => {
          const res = await fetch("https://api.escuelajs.co/api/v1/products");
  
          products = await res.json();
+
+         const res2 = await fetch("https://api.escuelajs.co/api/v1/categories");
+ 
+         categories = await res2.json();
         
         }
         catch{
@@ -23,21 +28,41 @@ exports.find = (id) => {
 
  }
 
- exports.create = (p) => { 
-    
-    products.push(p);
+ exports.create = (p) => {
 
-    return p;
+    let productSchema = {
+        "id": new Date().valueOf(),
+        "title": "",
+        "price": 100,
+        "description": "",
+        "images": [],
+        "creationAt": new Date(),
+        "updatedAt": new Date(),
+        "category": {}
+      }
+
+    Object.keys(productSchema).forEach( k => {productSchema[k] = p[k] || productSchema[k]} );
+    
+    productSchema.category = categories.find( c => c.id === p.categoryId )
+
+    products.push(productSchema);
+
+    return productSchema;
 
  }
 
  exports.update = (id , p) => { 
-   let i = products.findIndex( p => p.id==id );
 
-    products[i] = p;
+    let i = products.findIndex( p => p.id==id );
 
-    return products[i];
+    if (i === -1){return "There is no product with this id";}
+    else{
+        Object.keys(products[i]).forEach( k => {products[i][k] = p[k] || products[i][k]} );
 
+        products[i].updatedAt = new Date();
+
+        return products[i];
+        }
  }
 
  exports.delete = (id) => { 
